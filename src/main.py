f1a9f0778087e3c9ae5136e883b1995de928f70f
Pigioty <pigiotyreal@gmail.com>
2023-12-13 21:01:00 -0500

    display word (for debugging)

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from colorama import init, Fore, Back, Style
 from words import words
+import random
 
 init(autoreset=True)
+word = random.choice(list(words))
 
-print("Welcome to Wordle-Term!")
+print(Fore.GREEN + "Welcome to Wordle-Term!")
+print(Fore.LIGHTBLUE_EX + f"word: {word}")
