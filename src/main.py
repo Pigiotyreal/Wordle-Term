aa14b238c9198454fb97fdbf54585cbc44d69aee
Pigioty <pigiotyreal@gmail.com>
2023-12-13 21:22:40 -0500

    show word option

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
 from colorama import init, Fore
 from words import words
 import random
+import sys
 
 init(autoreset=True)
+    
 word = random.choice(list(words))
 
 print(Fore.GREEN + "Welcome to Wordle-Term!")
 print(Fore.LIGHTMAGENTA_EX + "How to play: " + Fore.RESET + "\nGuess the five letter word in 6 tries or less!\nOnce you guess your first word, 3 colors will appear:\nGray: Letter is not in the word\nYellow: Letter is in the word, but not in the correct position\nGreen: Letter is in the word and in the correct position\nGood luck!\n")
-print(Fore.LIGHTBLUE_EX + f"word: {word}")
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == "--show-word":
+        print(Fore.LIGHTBLUE_EX + f"word: {word}")
 
 input("Press enter to continue...")
 print("\n" * 100)
